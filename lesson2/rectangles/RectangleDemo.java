644fc87c393087d408b8992cf598ccb24cc39647
Tareq Fadel <itrainit@gmail.com>
2015-06-17 13:58:31 +0100

    Construct 2 rectangles touching each other

--- lesson2/rectangles/RectangleDemo.java
+++ lesson2/rectangles/RectangleDemo.java
@@ -1,9 +1,10 @@
 public class RectangleDemo
 {
-    public static void main(String[] args)
+    public static void main(String args[])
     {
-        Rectangle box = new Rectangle(5, 10, 60, 90);
-        box.draw();
-        box.translate(200, 100);
+        Rectangle box1 = new Rectangle(60, 90, 20, 30);
+        Rectangle box2 = new Rectangle(80, 120, 20, 30);
+        box1.draw();
+        box2.draw();
     }
 }
