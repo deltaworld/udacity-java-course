14c04b9c1681314e5a2695d4ac0e1619c329c8b0
Tareq Fadel <itrainit@gmail.com>
2015-06-17 14:33:34 +0100

    Java Replace method

--- lesson2/strings/StringDemo.java
+++ lesson2/strings/StringDemo.java
@@ -3,7 +3,7 @@ public class StringDemo
     public static void main(String[] args)
     {
         String river = "Mississippi";
-        int numberOfLetters = river.length();
-        System.out.println(numberOfLetters);
+        String strReplace = river.replace("i", "x");
+        System.out.println(strReplace);
     }
 }
