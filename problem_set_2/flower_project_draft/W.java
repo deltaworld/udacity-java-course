c7e62cb47e44136c4d8b8bb33e9231b8573a12b3
Tareq Fadel <itrainit@gmail.com>
2015-06-21 11:33:11 +0100

    Problem Set 2: Inch Worm

---
+++ problem_set_2/flower_project_draft/W.java
@@ -0,0 +1,10 @@
+public class W
+{
+    public static void main(String[] args)
+    {
+       Line line1 = new Line(0, 0, 10, 30);
+       Line line2 = new Line(10, 30, 20, 0);
+       Line line3 = new Line(20, 0, 30, 30);
+       Line line4 = new Line(30, 30, 40, 0);
+    }
+}
