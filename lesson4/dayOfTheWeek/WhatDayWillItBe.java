e8845e610769a66a053faaeda122a1f8677dd48c
Tareq Fadel <itrainit@gmail.com>
2015-06-22 11:04:28 +0100

    Remove Case Statement

--- lesson4/dayOfTheWeek/WhatDayWillItBe.java
+++ lesson4/dayOfTheWeek/WhatDayWillItBe.java
@@ -14,28 +14,27 @@ public class WhatDayWillItBe
         // Compute the weekday: 0 - Sunday, 1 - Monday, ..., 6 - Saturday
         Day startDay = new Day(1900, 1, 1);
         
-        int dayOfWeek = birthday.daysFrom(startDay) % 7 - 1;
+        int weekday = birthday.daysFrom(startDay) % 7 - 1;
         
-;
-        String weekday;
-        switch (dayOfWeek) {
-            case 0:  weekday = "Sunday";
-                     break;
-            case 1:  weekday = "Monday";
-                     break;
-            case 2:  weekday = "Tuesday";
-                     break;
-            case 3:  weekday = "Wednesday";
-                     break;
-            case 4:  weekday = "Thursday";
-                     break;
-            case 5:  weekday = "Friday";
-                     break;
-            case 6:  weekday = "Saturday";
-                     break;
-            default: weekday = "Invalid day";
-                     break;
-        }
+//         String weekday;
+//         switch (dayOfWeek) {
+//             case 0:  weekday = "Sunday";
+//                      break;
+//             case 1:  weekday = "Monday";
+//                      break;
+//             case 2:  weekday = "Tuesday";
+//                      break;
+//             case 3:  weekday = "Wednesday";
+//                      break;
+//             case 4:  weekday = "Thursday";
+//                      break;
+//             case 5:  weekday = "Friday";
+//                      break;
+//             case 6:  weekday = "Saturday";
+//                      break;
+//             default: weekday = "Invalid day";
+//                      break;
+//         }
         
         
         
