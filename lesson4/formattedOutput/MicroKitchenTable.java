2a6846176a6a2808312684f294a488191d2a5200
Tareq Fadel <itrainit@gmail.com>
2015-06-22 14:19:54 +0100

    Formatted Output

--- lesson4/formattedOutput/MicroKitchenTable.java
+++ lesson4/formattedOutput/MicroKitchenTable.java
@@ -11,14 +11,25 @@ public class MicroKitchenTable
         int cayCookiesPerDay = 1;
         double cayCerealPerDay = 0;
         String cayName = "Cay";
+        System.out.println("");
         System.out.println("cookies | cereal | name");
         System.out.println("------------------------");
 
         // TODO: Print entries for Sara
         // Be sure to line up the entries in the columns using printf
         // Print floating-point numbers with two digits after the decimal point
-
+        System.out.printf("%8d", saraCookiesPerDay);
+        System.out.print("|");
+        System.out.printf("%8.2f", saraCerealPerDay);
+        System.out.print("|");
+        System.out.printf("%s", saraName);
         // TODO: Print entries for Cay
+        System.out.println("");
+        System.out.printf("%8d", cayCookiesPerDay);
+        System.out.print("|");
+        System.out.printf("%8.2f", cayCerealPerDay);
+        System.out.print("|");
+        System.out.printf("%s", cayName);
 
     }
 }
