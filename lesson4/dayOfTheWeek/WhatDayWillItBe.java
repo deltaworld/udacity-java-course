babe64c230cbc516c0da37bd9d909351662f9487
Tareq Fadel <itrainit@gmail.com>
2015-06-22 11:02:17 +0100

    Day of Week - Case Statement

--- lesson4/dayOfTheWeek/WhatDayWillItBe.java
+++ lesson4/dayOfTheWeek/WhatDayWillItBe.java
@@ -12,6 +12,33 @@ public class WhatDayWillItBe
         // January 1, 1900 was a Monday
         // Compute the number of days from that day until the birthday
         // Compute the weekday: 0 - Sunday, 1 - Monday, ..., 6 - Saturday
+        Day startDay = new Day(1900, 1, 1);
+        
+        int dayOfWeek = birthday.daysFrom(startDay) % 7 - 1;
+        
+;
+        String weekday;
+        switch (dayOfWeek) {
+            case 0:  weekday = "Sunday";
+                     break;
+            case 1:  weekday = "Monday";
+                     break;
+            case 2:  weekday = "Tuesday";
+                     break;
+            case 3:  weekday = "Wednesday";
+                     break;
+            case 4:  weekday = "Thursday";
+                     break;
+            case 5:  weekday = "Friday";
+                     break;
+            case 6:  weekday = "Saturday";
+                     break;
+            default: weekday = "Invalid day";
+                     break;
+        }
+        
+        
+        
 
         System.out.print("Weekday: ");
         System.out.println(weekday);
