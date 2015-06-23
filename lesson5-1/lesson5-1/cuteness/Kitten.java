20b2d95d7c2e88a1608642b439aa8dcdde79947b
Tareq Fadel <itrainit@gmail.com>
2015-06-23 20:53:34 +0100

    Cuteness App

--- lesson5-1/lesson5-1/cuteness/Kitten.java
+++ lesson5-1/lesson5-1/cuteness/Kitten.java
@@ -1,32 +1,37 @@
+// Bluej project: lesson5/cuteness
+
 public class Kitten
 {
-    private int months; // Age. 
-    public Kitten(int ageInMonths) 
+    private int months; // Age.
+    
+    public Kitten(int ageInMonths)
     {
         months = ageInMonths;
     }
 
     /**
-     * Returns how cute a kitten is based on age. 
-     * Kittens can be "cute", "super cute", or "not so cute". 
+     * Returns how cute a kitten is based on age.
+     * Kittens can be "cute", "super cute", or "not so cute".
      * @return kittens below 2 months are not so cute. kittens
-     *         above 6 months are cute. kittens between 2 and 
-     *         6 months (inclusive) are super cute. 
+     *         above 6 months are cute. kittens between 2 and
+     *         6 months (inclusive) are super cute.
      */
-    public String getCuteness() 
+    public String getCuteness()
     {
         String cuteness = "";
-        if (months > 6)
-        {
-            cuteness = "cute";
-        }
-        else if (months >=2)
-        {
-            cuteness = "super cute";
-        }
-        else
+
+        // TODO: add the code to determine the cuteness
+        if (months < 2)
         {
             cuteness = "not so cute";
+        } 
+        else if (months > 6)
+        {
+            cuteness = "cute";
+        } 
+        else
+        {
+            cuteness = "super cute";
         }
         return cuteness;
     }
