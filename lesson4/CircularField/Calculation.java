040e05d8a16344d1c315fcaee5cc865dd444a19c
Tareq Fadel <itrainit@gmail.com>
2015-06-23 19:37:32 +0100

    Lesson 5: ElevatorDemo Example

--- lesson4/CircularField/Calculation.java
+++ lesson4/CircularField/Calculation.java
@@ -6,5 +6,13 @@ public class Calculation
         double height = 800;
         
         System.out.println(height + 4*number);
+        
+        // rows = (int)((length - 2*radius) / (radius * sqrt(3))) + 1;
+        // colsOdd = (int)(width / (radius*2))
+        // colsEve = (int)((width - radius)/ (radius*2))
+        // oddRows = (int)((rows+1)/2)
+        // eveRows = (int)(rows/2)
+        // noOnRows = (int)((total/2) + total%2)
+        // totalCircles = numOddRows * circlesPerOddRow + numEvenRows * circlesPerEvenRow
     }
 }
