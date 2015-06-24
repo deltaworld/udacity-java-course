9c907af4f3ba3724dfd40d44cbd79a8a31e10e50
Tareq Fadel <itrainit@gmail.com>
2015-06-24 23:33:43 +0100

    Amend algorithm to sum odd numbers in card

---
+++ lesson6-1/billionaire2/Loop.java
@@ -0,0 +1,12 @@
+public class Loop
+{
+    public static void main(String[] args)
+    {
+        int n = 2;
+        while (n < 1000)
+        {
+            n = 10 * n;
+            System.out.println(n);
+        }
+    }
+}
