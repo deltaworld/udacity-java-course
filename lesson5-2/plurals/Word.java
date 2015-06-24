9f23f762b4b5f1f62918fae92455905ee01ee8f2
Tareq Fadel <itrainit@gmail.com>
2015-06-24 20:04:27 +0100

    Add Comment to Else

--- lesson5-2/plurals/Word.java
+++ lesson5-2/plurals/Word.java
@@ -51,6 +51,10 @@ public class Word
         {
             pluralWord += "es";
         }
+        else 
+        {
+            pluralWord += "s";
+        }
         return pluralWord;
     
     }
