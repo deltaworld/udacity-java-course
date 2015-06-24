308b0157af1735e2a3b1b4b13b2bde5b26b28fc0
Tareq Fadel <itrainit@gmail.com>
2015-06-24 23:44:58 +0100

    Credit Card Algorithm Pseudo-code to Java

--- lesson6-1/credit_card/CreditCard.java
+++ lesson6-1/credit_card/CreditCard.java
@@ -1,7 +1,7 @@
-// Bluej project: lesson6/credit_card1
-// Video: Sum Every Other Digit
+// Bluej project: lesson6/credit_card
+// Video: Credit Card Checksum 
 
-// TODO: Implement the sumCertainDigits method.
+// TODO: Implement the isValid method.
 public class CreditCard
 {
     private long cardNumber;
@@ -12,35 +12,50 @@ public class CreditCard
     }
 
     /**
-     * Calculates the sum of ever other digit in cardNumber 
-     * starting from the rightmost digit.
-     * @return the sum of every other digit starting from the rightmost.
+     * Calculates whether this CreditCard has a valid number.
+     * @return true if the number is valid, false if it's not.
      */
-    public int sumCertainDigits()
+    public boolean isValid()
     {
-        // So that we don't accidentally change the credit card number,
-        // make a copy called n. 
+        /* Pseudocode for isValid:
+         * sum = 0
+         * count = 0
+         * for each digit starting from the right
+         *     count ++
+         *     if count is odd
+         *         sum = sum + digit
+         *     else if (digit < 5)
+         *         sum = sum + 2 * digit
+         *     else
+         *         sum = sum + 2 * digit - 9
+         * if the last digit of the sum is zero
+         *     The card number is valid
+         */
         long n = cardNumber;
-        
-        // TODO this is the code from the a previous video. You can use it
-        // as a starting point, but you will need to change a lot.
-        // Change this method so that it will calculate the sum of every
-        // second digit instead of all the digits, and then return this 
-        // special sum. 
-        // for example, if n is 12345, this method should return the sum 
-        // 5 + 3 + 1
-        // There are links to videos with hints above the submit button. 
-        // Use them if you want more inspiration.
         int sum = 0;
-        
+        int count = 0;
+        // TODO this is the code from the last question. you can use it
+        // as a starting point, but you will need to change most of it.
         while (n > 0)
         {
-            int digit = (int)(n % 10); 
-            sum = sum + digit;
-            n = n/100;
+            int digit = (int)(n % 10); // 
+            n = n/10;
+            
+            count++;
+            if (count % 2 != 0) 
+            {
+                sum = sum + digit;
+            }
+            else if (digit < 5)
+            {
+                sum = sum + 2 * digit;
+            }
+            else
+            {
+                sum = sum + 2 * digit - 9;
+            }
+            
         }
-
-        System.out.println(sum);
-        return sum;
+        if (sum % 10 == 0) { return true;} else { return false;}
     }
 }
